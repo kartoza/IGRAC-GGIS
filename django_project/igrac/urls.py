c1af54a9fca29fdae108e8a554319978bb6f60b8
meomancer <meomancer@gmail.com>
2020-12-15 15:11:06 +0700

    add API to return html of metadata detail

--- django_project/igrac/urls.py
+++ django_project/igrac/urls.py
@@ -8,12 +8,15 @@ from .views import HomeView, map_view_with_slug
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
+from igrac.views import map_slug_metadata_detail
 
 urlpatterns = [
     url(r'^$',
         view=HomeView.as_view(),
         name='home_igrac'),
+    url(r'^view/(?P<slug>[^/]+)/metadata_detail/article$',
+        map_slug_metadata_detail,
+        name='map_view_slug_metadata_detail'),
     url(r'^view/(?P<slug>[^/]+)$',
         view=map_view_with_slug,
         name='map_view_slug'),
--- django_project/igrac/views.py
+++ django_project/igrac/views.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.http import Http404
+from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView
 from geonode.maps.views import map_view
+from geonode.groups.models import GroupProfile
+from geonode.monitoring import register_event
+from geonode.monitoring.models import EventType
 from .models.map_slug import MapSlugMapping
 
 
@@ -18,3 +24,28 @@ def map_view_with_slug(request, slug):
         raise Http404("Map does not exist")
 
     return map_view(request, mapid=map.map.id)
+
+
+def map_slug_metadata_detail(
+        request,
+        slug,
+        template='maps/slug/metadata_detail_article.html'):
+    try:
+        map = MapSlugMapping.objects.get(slug=slug)
+        map_obj = map.map
+    except MapSlugMapping.DoesNotExist:
+        raise Http404("Map does not exist")
+    group = None
+    if map_obj.group:
+        try:
+            group = GroupProfile.objects.get(slug=map_obj.group.name)
+        except GroupProfile.DoesNotExist:
+            group = None
+    site_url = settings.SITEURL.rstrip('/') if settings.SITEURL.startswith('http') else settings.SITEURL
+    register_event(request, EventType.EVENT_VIEW_METADATA, map_obj)
+    return render(request, template, context={
+        "link_online": reverse('map_view_slug', args=[slug]),
+        "resource": map_obj,
+        "group": group,
+        'SITEURL': site_url
+    })
