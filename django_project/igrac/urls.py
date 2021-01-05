dffaa17b84d869d797c7eacbab35b20ce5139f2a
meomancer <meomancer@gmail.com>
2021-01-05 10:45:07 +0700

    update metadata detail and add metadata detail for maps

--- django_project/igrac/urls.py
+++ django_project/igrac/urls.py
@@ -8,15 +8,18 @@ from .views import HomeView, map_view_with_slug
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-from igrac.views import map_slug_metadata_detail
+from igrac.views import MapSlugMetadataDetail, MapMetadataDetail
 
 urlpatterns = [
     url(r'^$',
         view=HomeView.as_view(),
         name='home_igrac'),
-    url(r'^view/(?P<slug>[^/]+)/metadata_detail/article$',
-        map_slug_metadata_detail,
+    url(r'^view/(?P<id>[^/]+)/metadata_detail/article$',
+        MapSlugMetadataDetail.as_view(),
         name='map_view_slug_metadata_detail'),
+    url(r'^maps/(?P<id>[^/]+)/metadata_detail/article$',
+        MapMetadataDetail.as_view(),
+        name='map_view_metadata_detail'),
     url(r'^view/(?P<slug>[^/]+)$',
         view=map_view_with_slug,
         name='map_view_slug'),
--- django_project/igrac/views.py
+++ django_project/igrac/views.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from django.http import Http404
-from django.shortcuts import render
-from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
+
+from geonode.maps.models import Map
 from geonode.maps.views import map_view
 from geonode.groups.models import GroupProfile
 from geonode.monitoring import register_event
 from geonode.monitoring.models import EventType
-from .models.map_slug import MapSlugMapping
 from geonode.documents.models import get_related_documents
+
+from igrac.models.map_slug import MapSlugMapping
 
 
 class HomeView(ListView):
@@ -27,27 +28,52 @@ def map_view_with_slug(request, slug):
     return map_view(request, mapid=map.map.id)
 
 
-def map_slug_metadata_detail(
-        request,
-        slug,
-        template='maps/slug/metadata_detail_article.html'):
-    try:
-        map = MapSlugMapping.objects.get(slug=slug)
-        map_obj = map.map
-    except MapSlugMapping.DoesNotExist:
-        raise Http404("Map does not exist")
-    group = None
-    if map_obj.group:
+class MetadataDetail(TemplateView):
+    """ View for returning template that showing map metadata"""
+    template_name = 'maps/metadata_detail_article.html'
+
+    def get_map_object(self, id):
+        """ Return map object from id
+        """
+        raise NotImplementedError
+
+    def get_context_data(self, **kwargs):
+        context = super(MetadataDetail, self).get_context_data(**kwargs)
+        id = kwargs['id']
+        map = self.get_map_object(id)
+        group = None
+        if map.group:
+            try:
+                group = GroupProfile.objects.get(slug=map.group.name)
+            except GroupProfile.DoesNotExist:
+                group = None
+        site_url = settings.SITEURL.rstrip('/') if settings.SITEURL.startswith('http') else settings.SITEURL
+        register_event(self.request, EventType.EVENT_VIEW_METADATA, map)
+        context.update({
+            "resource": map,
+            "group": group,
+            'SITEURL': site_url,
+            "documents": get_related_documents(map)
+        })
+        return context
+
+
+class MapSlugMetadataDetail(MetadataDetail):
+    def get_map_object(self, id):
+        """ Return map object from id
+        """
         try:
-            group = GroupProfile.objects.get(slug=map_obj.group.name)
-        except GroupProfile.DoesNotExist:
-            group = None
-    site_url = settings.SITEURL.rstrip('/') if settings.SITEURL.startswith('http') else settings.SITEURL
-    register_event(request, EventType.EVENT_VIEW_METADATA, map_obj)
-    return render(request, template, context={
-        "link_online": reverse('map_view_slug', args=[slug]),
-        "resource": map_obj,
-        "group": group,
-        'SITEURL': site_url,
-        "documents": get_related_documents(map_obj)
-    })
+            map = MapSlugMapping.objects.get(slug=id)
+            return map.map
+        except MapSlugMapping.DoesNotExist:
+            raise Http404("Map does not exist")
+
+
+class MapMetadataDetail(MetadataDetail):
+    def get_map_object(self, id):
+        """ Return map object from id
+        """
+        try:
+            return Map.objects.get(id=id)
+        except MapSlugMapping.DoesNotExist:
+            raise Http404("Map does not exist")
