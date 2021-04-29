c0ebf1853f033158593bbbc37b78f0c10527b65e
meomancer <meomancer@gmail.com>
2021-04-29 12:00:19 +0700

    Add google analytics

--- django_project/igrac/context_processors.py
+++ django_project/igrac/context_processors.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from .utilities import get_default_filter_by_group
 
 
-def resource_urls(request):
+def extra_context(request):
     """Global values to pass to templates"""
 
     defaults = dict(
-        DEFAULT_GROUP_FILTER=get_default_filter_by_group(request.user)
+        DEFAULT_GROUP_FILTER=get_default_filter_by_group(request.user),
+        GOOGLE_ANALYTIC_KEY=settings.GOOGLE_ANALYTIC_KEY
     )
 
     return defaults
