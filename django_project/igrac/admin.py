139b0600542e187091ade7b223a52ad65cf80bb1
meomancer <meomancer@gmail.com>
2022-02-18 10:00:42 +0700

    Add make active action in user admin

--- django_project/igrac/admin.py
+++ django_project/igrac/admin.py
@@ -1,7 +1,11 @@
 from preferences.admin import PreferencesAdmin
 from adminsortable.admin import SortableAdmin
 from django.contrib import admin
-from .models.map_slug import MapSlugMapping
+
+from geonode.base.admin import set_user_and_group_layer_permission
+from geonode.people.admin import ProfileAdmin
+from geonode.people.models import Profile
+from igrac.models.map_slug import MapSlugMapping
 from igrac.models.site_preference import SitePreference
 
 
@@ -12,3 +16,18 @@ class MapSlugMappingAdmin(SortableAdmin):
 
 admin.site.register(MapSlugMapping, MapSlugMappingAdmin)
 admin.site.register(SitePreference, PreferencesAdmin)
+
+admin.site.unregister(Profile)
+
+
+def make_active(modeladmin, request, queryset):
+    for profile in queryset:
+        profile.is_active = True
+        profile.save()
+
+
+class IgracProfileAdmin(ProfileAdmin):
+    actions = [set_user_and_group_layer_permission, make_active]
+
+
+admin.site.register(Profile, IgracProfileAdmin)
