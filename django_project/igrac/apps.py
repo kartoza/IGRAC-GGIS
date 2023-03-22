d145f765bdb34a837344223226545ca4b86bc866
dimasciput <dimas.ciputra@gmail.com>
2023-03-22 10:01:52 +0700

    Update footer

--- django_project/igrac/apps.py
+++ django_project/igrac/apps.py
@@ -7,3 +7,14 @@ class IgracConfig(AppConfig):
     def ready(self):
         import igrac.signals
         import igrac.monkeypatch
+        from django.conf import settings
+        geonode_mapstore_client_template_dir = ''
+        for template_dir in settings.TEMPLATES[0]['DIRS']:
+            if 'geonode_mapstore_client' in template_dir:
+                geonode_mapstore_client_template_dir = template_dir
+                settings.TEMPLATES[0]['DIRS'].remove(template_dir)
+                break
+        if geonode_mapstore_client_template_dir:
+            settings.TEMPLATES[0]['DIRS'].append(
+                geonode_mapstore_client_template_dir
+            )
