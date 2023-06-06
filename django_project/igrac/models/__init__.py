a72485d898d94b7687fc136197ca8ad76b3bf0a7
Irwan Fathurrahman <meomancer@gmail.com>
2023-06-06 09:08:32 +0700

    Duplication layer of groundwater for specific organisations (#564)
    * Add duplication layer
    
    * Groundwater layer duplication on admin
    
    * Add layer source for duplication on preferences

--- django_project/igrac/models/__init__.py
+++ django_project/igrac/models/__init__.py
@@ -2,5 +2,6 @@
 from .map_slug import *
 from .blog import *
 from .profile import *
+from .groundwater_layer import *
 from igrac.models.manual_page import *  # noqa
 from igrac.models.site_preference import *  # noqa
