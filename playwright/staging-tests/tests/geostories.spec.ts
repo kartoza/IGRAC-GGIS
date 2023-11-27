95503933e4937a886067c4d3765954843fc9f372
Jeff Osundwa <jeff@kartoza.com>
2023-11-27 23:52:30 +0300

    Updates on testing

--- playwright/staging-tests/tests/geostories.spec.ts
+++ playwright/staging-tests/tests/geostories.spec.ts
@@ -7,7 +7,7 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'GeoStories' }).click();
   await page.waitForLoadState('domcontentloaded');
   await expect(page.locator('label').filter({ hasText: 'GeoStories' })).toBeVisible({timeout: 30000});
-  await page.getByText('2 Resources found').click();
+  await expect(page.getByText('2 Resources found')).toBeVisible();
   
   await page.locator('li').filter({ hasText: 'Part 2: The History of' }).getByRole('link').first().click();
   await expect(page.locator('li').filter({ hasText: 'Part 2: The History of' }).getByRole('link').first()).toBeVisible({timeout: 30000});
@@ -21,6 +21,7 @@ test('test', async ({ page }) => {
   await page.locator('div').filter({ hasText: /^View full metadata$/ }).nth(1).click();
   await expect(page.getByRole('heading', { name: 'Metadata : Part 1: Discover' })).toBeVisible({timeout: 30000});
   await page.goBack();
+  
   await expect(page.locator('li').filter({ hasText: 'Part 2: The History of' }).getByRole('link').first()).toBeVisible({timeout: 30000});
   await page.getByRole('link', { name: 'Part 2: The History of' }).click();
   await expect(page.getByRole('tabpanel').getByText('Part 2: The History of')).toBeVisible({timeout: 30000});
