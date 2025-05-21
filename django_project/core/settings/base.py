19fb0c21bcc3e23fda80bd76069f898725e3fe07
Irwan Fathurrahman <meomancer@gmail.com>
2025-05-21 09:53:01 +0700

    Enable resume all uploader

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -187,10 +187,10 @@ if 'gwml2' in INSTALLED_APPS:
     }
     # TODO:
     #  We enable this after resume being improved
-    # CELERY_BEAT_SCHEDULE['resume_all_uploader'] = {
-    #     'task': 'gwml2.tasks.upload_session.resume_all_uploader',
-    #     'schedule': crontab(minute='*/10'),
-    # }
+    CELERY_BEAT_SCHEDULE['resume_all_uploader'] = {
+        'task': 'gwml2.tasks.upload_session.resume_all_uploader',
+        'schedule': crontab(minute='*/10'),
+    }
 
 GWML2_FOLDER = os.getenv(
     'GWML_FOLDER', os.path.join(PROJECT_ROOT, 'gwml2-file')
