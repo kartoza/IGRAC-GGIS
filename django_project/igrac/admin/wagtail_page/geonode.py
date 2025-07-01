37fd4d6fa78215a0803aec4c3a61c54dae9ae5cc
Irwan Fathurrahman <meomancer@gmail.com>
2025-07-01 16:55:19 +0700

    Add datasets and maps on admin of geonode page

--- django_project/igrac/admin/wagtail_page/geonode.py
+++ django_project/igrac/admin/wagtail_page/geonode.py
@@ -1,10 +1,27 @@
 from adminsortable.admin import SortableAdmin
 from django.contrib import admin
+from django.utils.html import format_html
 
 from igrac.models import GeonodePage
 
 
 @admin.register(GeonodePage)
 class GeonodePageAdmin(SortableAdmin):
-    list_display = ('title', 'slug')
+    list_display = ('title', 'slug', '_datasets', '_maps')
     filter_horizontal = ('maps', 'datasets')
+
+    def _datasets(self, obj: GeonodePage):
+        return format_html(
+            ''.join([
+                f'<span style="display:inline-block; background:#ddd; margin:2px; padding: 4px 6px">{resource.title}</span>'
+                for resource in obj.datasets.all()
+            ])
+        )
+
+    def _maps(self, obj: GeonodePage):
+        return format_html(
+            ''.join([
+                f'<span style="display:inline-block; background:#ddd; margin:2px; padding: 4px 6px">{resource.title}</span>'
+                for resource in obj.maps.all()
+            ])
+        )
