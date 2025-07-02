225c6837028dd760a5f66ceee125c326d278034b
Irwan Fathurrahman <meomancer@gmail.com>
2025-07-02 11:54:34 +0700

    Add popup to geostory and documents (#899)
    * Add popup to geostory and documents
    
    * Bump commit version

--- django_project/igrac/models/wagtail_page/geonode.py
+++ django_project/igrac/models/wagtail_page/geonode.py
@@ -4,6 +4,8 @@ from wagtail.contrib.routable_page.models import route
 from wagtailautocomplete.edit_handlers import AutocompletePanel
 
 from geonode.maps.models import Map, Dataset
+from geonode.documents.models import Document
+from geonode.geoapps.models import GeoApp
 from igrac.models.wagtail_page.blog import BlogPage
 
 
@@ -11,10 +13,14 @@ class GeonodePage(BlogPage):
     """Blog map page is used for showing the blog when map is opened."""
     maps = ParentalManyToManyField(Map, blank=True)
     datasets = ParentalManyToManyField(Dataset, blank=True)
+    documents = ParentalManyToManyField(Document, blank=True)
+    geostories = ParentalManyToManyField(GeoApp, blank=True)
 
     content_panels = BlogPage.content_panels + [
         AutocompletePanel('maps', target_model=Map),
-        AutocompletePanel('datasets', target_model=Dataset)
+        AutocompletePanel('datasets', target_model=Dataset),
+        AutocompletePanel('documents', target_model=Document),
+        AutocompletePanel('geostories', target_model=GeoApp),
     ]
 
     @route(r'^body-only/$')
